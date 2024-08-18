de6fb33f6c9f913f0ce2061087d8efba5cb3a7d2
Sergio <sergio.pm.developer@gmail.com>
2024-08-18 20:25:21 +0000

    test: add e2e test for sign out functionality

--- tests/e2e/test_authentication.py
+++ tests/e2e/test_authentication.py
@@ -181,3 +181,21 @@ def test_sign_up_post_success(client: Client):
 
     assert response.status_code == 204
     assert client.session["_auth_user_id"] is not None
+
+
+@pytest.mark.django_db
+def test_sign_out_get_success(client: Client):
+    """
+    Tests the GET method of the sign out view and checks if the
+    response status code is 204 and the user is signed out
+    """
+
+    user = AppUser.objects.create_user(
+        username="username", email="user@email.com", password="password"
+    )
+    client.force_login(user)
+
+    response = client.get(reverse("sign-out"))
+
+    assert response.status_code == 204
+    assert client.session.get("_auth_user_id") is None
