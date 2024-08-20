aeb8a8dad3dd935983f00d4804ec58a0bb3b81bf
Sergio <sergio.pm.developer@gmail.com>
2024-08-20 23:26:29 +0000

    test: implement unit tests of the email confirmation tokens class

--- tests/unit/test_authentication.py
+++ tests/unit/test_authentication.py
@@ -2,6 +2,7 @@ import pytest
 from django.test import RequestFactory
 
 from authentication.models import AppUser
+from authentication.utils.email.tokens import EmailConfirmationTokens, TokenError
 from authentication.utils.sign_in import SignIn
 from authentication.utils.sign_up import SignUp
 
@@ -156,3 +157,37 @@ def test_sign_up_validate_user_valid_credentials(rf: RequestFactory):
 
     assert sign_up.errors == []
     assert sign_up.user is not None
+
+
+def test_email_confirmation_tokens_generate_token():
+    """
+    Tests the generate_token function of the EmailConfirmationTokens
+    class and checks if the token is generated correctly
+    """
+
+    token = EmailConfirmationTokens.generate_token(user_id=1)
+
+    assert isinstance(token, str)
+
+
+def test_email_confirmation_tokens_validate_token_invalid_token():
+    """
+    Tests the validate_token function of the EmailConfirmationTokens
+    class with an invalid token and checks if the TokenError exception is raised
+    """
+
+    with pytest.raises(TokenError):
+        EmailConfirmationTokens.validate_token(token="invalid_token")
+
+
+def test_email_confirmation_tokens_validate_token_valid_token():
+    """
+    Tests the validate_token function of the EmailConfirmationTokens
+    class with a valid token and checks if the user id is returned correctly
+    """
+
+    token = EmailConfirmationTokens.generate_token(user_id=1)
+
+    user_id = EmailConfirmationTokens.validate_token(token=token)
+
+    assert user_id == 1
